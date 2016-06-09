00a716f2c699410e01312a4f58a754da7e5aeae7
Hafeez Syed <hafeez.syed@team.telstra.com>
2016-06-10 09:47:03 +1000

    testing textarea content validation

--- 03-let,const,block-scoping/js/script.js
+++ 03-let,const,block-scoping/js/script.js
@@ -1,4 +1,51 @@
 'use strict';
+var cursorPosition = null,
+    typingInterval = 500,
+    found = false,
+    typingTimer;
+$(".additional-notes").keyup(validateComments);
+$(".additional-notes").keydown(function(e) {
+    cursorPosition = this.selectionEnd;
+});
+function validateComments(e) {
+    var regExp = /[^A-Z0-9\.\s\r\n\'\"\@\$\!\(\)\-\,]+/igm,
+        val = $(this).val(),
+        limit = 500,
+        start = this.selectionStart,
+        end = this.selectionEnd;
+
+    //console.log("START = "+start)
+    //console.log("END = "+end)
+    //console.log("LENGTH = "+val.length);
+    //console.log("\n\n");
+    var test  = regExp.test(this.value);
+    if(test) {
+        if(end < val.length) {
+            console.log(false);
+            console.log(cursorPosition);
+            found = true;
+            //cursorPosition = end;
+            //end = cursorPosition;
+        }
+    }
+    this.value = this.value.replace(regExp, '');
+    this.setSelectionRange(start, end);
+
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, typingInterval);
+
+    //if(val.length > limit) {
+        //val = val.substr(0, limit);
+    //}
+    //return $(this).val(val);
+}
+
+function doneTyping() {
+    if(found) {
+        $(".additional-notes").setSelectionRange(cursorPosition, cursorPosition);
+        found = false;
+    }
+}
 
 console.log(productId, 'var');
 var productId = 12;
@@ -17,3 +64,5 @@ let id = 12;
 console.log(id);
 
 //*************//
+
+
