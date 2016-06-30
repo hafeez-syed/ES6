d6488e841c2f079fcf486bb12473f89808435b2f
Hafeez Syed <hafeez.syed@team.telstra.com>
2016-06-30 15:59:44 +1000

    adding arrow functions

--- 03-let,const,block-scoping/js/script.js
+++ 03-let,const,block-scoping/js/script.js
@@ -2,20 +2,22 @@
 
 consoleAndDisplayResults(productId, 'var');
 var productId = 12;
+var divId = 'var';
 
 //*************//
 
-//consoleAndDisplayResults(pId, 'let');
+//consoleAndDisplayResults(pId, divId);
 let pId = 12;
-
+divId = 'let';
+consoleAndDisplayResults('=> Uncaught ReferenceError: pId is not defined (uncomment the code to see the error in console.)', divId);
 //*************//
 
-
+divId = 'block1';
 let id = 12;
 {
     let id = 2000;
 }
-consoleAndDisplayResults(id, 'block1');
+consoleAndDisplayResults(id, divId);
 
 //*************//
 
@@ -23,62 +25,146 @@ function updateProductId() {
     memberId = 15;
 }
 
+divId = 'block2';
 let memberId = null;
 updateProductId();
-consoleAndDisplayResults(memberId, 'block2');
+consoleAndDisplayResults(memberId, divId);
 
 //*************//
 
+divId = 'block3';
 let userId = 42;
 for (let userId = 0; userId < 10; userId++) {
 }
-consoleAndDisplayResults(userId, 'block3');
+consoleAndDisplayResults(userId, divId);
 
 //*************//
 
 var updateVarFunctions = [];
+divId = 'block4';
 for (var i=0; i < 2; i++) {
     updateVarFunctions.push(function(){ return i;});
 }
 
-consoleAndDisplayResults(updateVarFunctions[0](), 'block4');
+consoleAndDisplayResults(updateVarFunctions[0](), divId);
 
-consoleAndDisplayResults('\n_________________________________________________________\n\n', 'block4');
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
 
 let updateLetFunctions = [];
 for (let i=0; i < 2; i++) {
     updateLetFunctions.push(function(){ return i;});
 }
 
-consoleAndDisplayResults(updateLetFunctions[0](), 'block4');
+consoleAndDisplayResults(updateLetFunctions[0](), divId);
 
+divId = 'block5'
 const MARKUP_PCT = 100;
-consoleAndDisplayResults(MARKUP_PCT, 'block5');
+consoleAndDisplayResults(MARKUP_PCT, divId);
 
-consoleAndDisplayResults('\n_________________________________________________________\n\n', 'block5');
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
 
 //const TOTAL_PRODUCTS;
 //consoleAndDisplayResults(TOTAL_PRODUCTS, 'block5');
 
-consoleAndDisplayResults("=> Uncaught SyntaxError: Missing initializer in const declaration. (uncomment the code to see the error in console.)", 'block5');
+consoleAndDisplayResults("=> Uncaught SyntaxError: Missing initializer in const declaration. (uncomment the code to see the error in console.)", divId);
 
-consoleAndDisplayResults('\n_________________________________________________________\n\n', 'block5');
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
 
 //MARKUP_PCT = 10;
 //consoleAndDisplayResults(MARKUP_PCT, 'block5');
-consoleAndDisplayResults("=> Uncaught TypeError: Assignment to constant variable. (uncomment the code to see the error in console.)", 'block5');
+consoleAndDisplayResults("=> Uncaught TypeError: Assignment to constant variable. (uncomment the code to see the error in console.)", divId);
 
-consoleAndDisplayResults('\n_________________________________________________________\n\n', 'block5');
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
 
 const CHILD_AGE = 5;
 if(CHILD_AGE > 0) {
     const CHILD_AGE = 7;
 }
-consoleAndDisplayResults(CHILD_AGE, 'block5');
+consoleAndDisplayResults(CHILD_AGE, divId);
 
 
 //*************//
 var getPrice = () => 5.99;
+divId = 'block6';
+
 consoleAndDisplayResults(typeof getPrice, 'block6');
-consoleAndDisplayResults('\n_________________________________________________________\n\n', 'block6');
-consoleAndDisplayResults(getPrice(), 'block6');
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+consoleAndDisplayResults(getPrice(), divId);
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+getPrice = count => count * 4;
+consoleAndDisplayResults(getPrice(4), divId);
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+getPrice = (count, tax) => count * 4 * (1 + tax);
+consoleAndDisplayResults(getPrice(2, 0.07), divId);
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+getPrice = (count, tax) => {
+    var price = count * 4.00;
+    price *= ( 1 + tax);
+    return price;
+}
+consoleAndDisplayResults(getPrice(2, 0.07), divId);
+
+divId = 'block7'
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+
+//*************//
+document.addEventListener('click', function() {
+    consoleAndDisplayResults('ES5 way will give you >#document', 'block7');
+});
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+
+document.addEventListener('click', () => consoleAndDisplayResults('ES6 way will give you '+ this, 'block7'));
+
+
+//*************//
+divId = 'block8';
+var invoice = {
+    number: 123,
+    process: function () {
+        consoleAndDisplayResults(JSON.stringify(this), divId);
+    }
+};
+invoice.process();
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+
+invoice = {
+    number: 123,
+    process: () => consoleAndDisplayResults(this, divId)
+};
+invoice.process();
+
+//*************//
+divId = 'block9';
+var invoice = {
+    number: 123,
+    process: function () {
+        return function() {
+            //consoleAndDisplayResults(this.number, divId);
+            consoleAndDisplayResults("=> Uncaught TypeError: Cannot read property 'number' of undefined", divId);
+        }
+    }
+};
+invoice.process()();
+consoleAndDisplayResults('\n_________________________________________________________\n\n', divId);
+
+invoice = {
+    number: 123,
+    process: function() {
+        return () => consoleAndDisplayResults(this.number, divId);
+    }
+};
+invoice.process()();
+
+
+//*************//
+divId = 'block10';
+var invoice = {
+    number: 123,
+    process: function() {
+        return () => consoleAndDisplayResults(this.number, divId);
+    }
+};
+var newInvoice = {
+    number: 456
+}
+invoice.process().bind(newInvoice)();
